60a5be1bdb16ba28cf42a0d618022d8de25bddde
varia <ltnhan2610@gmail.com>
2024-10-04 11:02:34 +0700

    change expirationDate datatype from date to timestamp

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,13 +3,12 @@ import {
 	mysqlTable,
 	timestamp,
 	varchar,
-	date,
 } from "drizzle-orm/mysql-core";
 
 export const shortCodes = mysqlTable("shortcodes", {
 	shortCode: varchar("shortcode", { length: 255 }).notNull().primaryKey(),
 	destination: text("destination").notNull(),
 	password: varchar("password", { length: 255 }),
-	expirationDate: date("expirationDate"),
+	expirationDate: timestamp("expirationDate"),
 	createdAt: timestamp("createdAt").defaultNow().notNull(),
 });
